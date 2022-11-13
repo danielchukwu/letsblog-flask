153ae8eaf39c790c892f23712072ea8ab1df2b0a
dc <00danzy@gmail.com>
2022-11-13 22:14:04 +0100

    first commit

---
+++ info.py
@@ -0,0 +1,77 @@
+import os
+
+
+os.environ["user"] = "postgres"
+os.environ["password"] = "Danielcarl4u_"
+
+content = """
+Lionel Messi has been universally recognized as the best soccer player in the world. His quick dribbling skills and elusive tricks on the ball put him in a class of his own.
+
+He is a goal scorer, play maker, and an electrifying talent at the young age of 24.
+
+But is he the best? Lets take a look at five reasons why the Argentinian is not the world's best player.
+
+No Right Foot
+1 OF 5
+Messi has a left foot that is sensational to any others. The way he strikes the ball and can cut quickly has proven to be unstoppable for many opponents. 
+
+Does he have the best left foot in the world? Yes, without question. So what's the problem?
+
+Messi is a one footed player and it hinders his play. His right foot is nowhere near up to par with his left, and it has led him to take some poor shots and sometimes causes him to lose the ball.
+
+Great players such as Messi's Barcelona teammate, David Villa and Manchester United's Wayne Rooney use both feet. To be the world's best, Messi needs to improve his right foot. 
+
+Never Played Outside of La Liga
+2 OF 5
+Lionel Messi plays for FC Barcelona of La Liga, undoubtedly one of the world's greatest football teams.
+
+La Liga, in Spain, has been known for its quick play, control and technical skill. Its teams have been known to play a "pretty" style of football that is perfect for Messi's talented foot skills.
+
+There is no problem playing in La Liga, it's actually one of the most respected leagues in the world. It's always been questionable though, if Lionel Messi could succeed in another league.
+
+Lets look at the Premier League in England for example. The Premier League is known for its aggressive and hard nosed played.
+
+Could Messi succeed in a much more physical league being only 5'6"?
+
+Well we may never know, but we know that Christiano Ronaldo can (played for Manchester United), and I'd be skeptical to see if Messi could do the same, especially against the likes of Terry and Vidic.  
+
+Plays with World's Best Center Midfield
+3 OF 5
+"Leo" Messi is privileged and skilled enough to star on FC Barcelona. His magical play, though, would not be possible with out the help of some teammates who are stars themselves.
+
+Messi is the benefactor of playing with the world's best center midfielders dishing him the ball. FC Barcelona studs, Xavi and Andres Iniesta, are hands down two of the best center midfielders in the world. They may even be the best and proved that while winning the 2010 World Cup in South Africa. 
+
+Messi is privileged to play in front of these two players and most of his goals benefit from their passes. We may never know who makes who look better or if Messi could do it without them. We do, however, know that Messi probably could not put up the goal numbers by himself. 
+
+World Cup Woes
+4 OF 5
+If you're the best player in the world, you should be able to take over the game and will your team to victory. I know there are 10 other players on your team, but if you are the world's best, then your team should have an edge.
+
+Messi is from the great country of Argentina, and they're great at soccer too. He is lucky to have other great players on his team such as, Carlos Tevez and Gonzalo Higuain. 
+
+Messi, unfortunately has not been able to lead Argentina to World Cup glory, even with the help of his teammates.
+
+Messi has participated in two World Cups and has just one goal in nine games. In those two World Cups, Argentina has been unable to get pass the quarterfinals and has under performed miserably.
+
+Even though Leo has done quite well as a playmaker in these games, it is hard to see if he has the intangibles to lead Argentina to a World Cup championship.
+
+If Messi was the greatest player in the world, then he should be able to lead his team to World Cup prominence. Maradona won a World Cup for Argentina, Messi needs to do the same to be the world's greatest. 
+
+Cristiano Ronaldo Is Better
+5 OF 5
+That's right I said it, Cristiano Ronaldo is a better soccer player than Leo Messi.
+
+Here's why:
+
+Ronaldo scored 40 goals last year in La Liga and Messi scored 31. Yes, I know that is not much of a discrepancy, but Ronaldo's goals overmatched Messi's.
+
+Ronaldo is able to score with his head, left or right footed, and is also a magician on the ball. Messi is simply poor in the air and can not head the ball.
+
+Not only that, but Messi does not have play with the aggressive pace that Ronaldo does. Ronaldo is much more of a physical force on the field and his eight extra inches on Messi, height wise, doesn't hurt. 
+
+Messi can take free kicks, but he is simply not as good as Ronaldo. Even though Ronaldo had a poor job converting free kicks this year, he has a long history of being one of the best at them and Messi is just not there yet. 
+
+All and all, Messi is a great football player but Ronaldo is superior, and that means Messi is not the world's greatest player.
+"""
+
+print(len(content))
